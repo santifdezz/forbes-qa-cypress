8b4339647b9bf12a4e7e91119a3678154aa45afa
Santiago Fernandez <sfernandezs22@fpcoruna.afundacion.org>
2024-06-20 11:27:57 +0200

    update of yml

--- cypress/functions/commonFunctions.js
+++ cypress/functions/commonFunctions.js
@@ -4,8 +4,8 @@ import {homeElements} from "../constants.js";
 export function visitPage() {
     cy.clearAllCookies();
     cy.visit(homeElements.HOME_URL, { failOnStatusCode: false });
-    getElement(homeElements.ACCEPT_COOKIES).as('acceptButton');
-    cy.get('@acceptButton').should('be.visible').click();
+   // getElement(homeElements.ACCEPT_COOKIES).as('acceptButton');
+   // cy.get('@acceptButton').should('be.visible').click();
 }
 export const onClick = (selector, force = false) => getElement(selector).first().click({ force });
 export const typeInput = (selector, text, force = false) => getElement(selector).type(text,{ force } );
